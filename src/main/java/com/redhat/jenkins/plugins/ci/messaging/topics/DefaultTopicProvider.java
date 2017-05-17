4211e17fd90760ea4deaeb69d58913aef16524c3
Greg Allen <gallen@redhat.com>
2017-05-17 10:24:48 -0400

    [CID-2212] Add topic provider to jms-messaging-plugin.

---
+++ src/main/java/com/redhat/jenkins/plugins/ci/messaging/topics/DefaultTopicProvider.java
@@ -0,0 +1,37 @@
+package com.redhat.jenkins.plugins.ci.messaging.topics;
+
+import hudson.Extension;
+import hudson.model.Descriptor;
+import jenkins.model.Jenkins;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+
+public class DefaultTopicProvider extends TopicProvider {
+
+    private static final long serialVersionUID = -8194184157003849025L;
+
+    @DataBoundConstructor
+    public DefaultTopicProvider() {}
+
+    @Override
+    public Descriptor<TopicProvider> getDescriptor() {
+        return Jenkins.getInstance().getDescriptorByType(DefaultTopicProviderDescriptor.class);
+    }
+
+    @Extension
+    public static class DefaultTopicProviderDescriptor extends TopicProviderDescriptor {
+
+        @Override
+        public String getDisplayName() {
+            return "Default Topic Provider";
+        }
+
+        public String generatePublisherTopic() {
+            return null;
+         }
+
+        public String generateSubscriberTopic() {
+            return null;
+         }
+    }
+}
