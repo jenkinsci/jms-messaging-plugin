0ae03c7befd59dea11518369d893618bc3e27061
Greg Allen <gallen@redhat.com>
2019-04-17 12:14:25 -0400

    Fix to handle pipelines.

--- src/main/java/com/redhat/jenkins/plugins/ci/threads/TriggerThreadTransientActionFactory.java
+++ src/main/java/com/redhat/jenkins/plugins/ci/threads/TriggerThreadTransientActionFactory.java
@@ -1,12 +1,13 @@
 package com.redhat.jenkins.plugins.ci.threads;
 
 import hudson.Extension;
-import hudson.model.AbstractProject;
+import hudson.model.Job;
 
 import java.util.Collection;
 import java.util.Collections;
 
 import jenkins.model.TransientActionFactory;
+import jenkins.model.ParameterizedJobMixIn;
 
 import com.redhat.jenkins.plugins.ci.CIBuildTrigger;
 
@@ -16,17 +17,12 @@ public class TriggerThreadTransientActionFactory extends TransientActionFactory 
 
     @Override
     public Class type() {
-        return AbstractProject.class;
+        return Job.class;
     }
 
     @Override
     public Collection createFor(Object target) {
-        if (target instanceof AbstractProject) {
-            AbstractProject project = (AbstractProject)target;
-            @SuppressWarnings("unchecked")
-            CIBuildTrigger cibt = (CIBuildTrigger)project.getTrigger(CIBuildTrigger.class);
-            return cibt == null || cibt.getJobAction() == null ? Collections.emptyList() : Collections.singletonList(cibt.getJobAction());
-        }
-        return Collections.emptyList();
+        CIBuildTrigger cibt = ParameterizedJobMixIn.getTrigger((Job<?,?>) target, CIBuildTrigger.class);
+        return cibt == null || cibt.getJobAction() == null ? Collections.emptyList() : Collections.singletonList(cibt.getJobAction());
     }
 }
