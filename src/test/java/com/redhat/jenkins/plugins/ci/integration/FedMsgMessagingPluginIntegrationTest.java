8452088233a8d0eee446fbd237057246895be29a
Scott Hebert <shebert@redhat.com>
2019-11-04 21:18:22 -0500

    Correct handling of Boolean Parameters during triggering
    Fixes #141

--- src/test/java/com/redhat/jenkins/plugins/ci/integration/FedMsgMessagingPluginIntegrationTest.java
+++ src/test/java/com/redhat/jenkins/plugins/ci/integration/FedMsgMessagingPluginIntegrationTest.java
@@ -247,13 +247,6 @@ public class FedMsgMessagingPluginIntegrationTest extends SharedMessagingPluginI
         jenkins.save();
     }
 
-    @WithPlugins("workflow-aggregator")
-    @Test
-    public void testSimpleCIEventTriggerWithBooleanParam() {
-        _testSimpleCIEventTriggerWithBoolParam("scott=123\ntom=456",
-                "dryrun is false");
-    }
-
     @Test
     public void testTriggerWithComplexCheck() throws Exception {
 
